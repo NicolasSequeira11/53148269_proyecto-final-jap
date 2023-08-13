5dfef7d62e72517817437960501d2f96be9af981
snunes92 <82555961+snunes92@users.noreply.github.com>
2023-08-13 17:14:35 -0300

    redireccion al login en caso de no estar logueado
    implemente el codigo en login.js y lo linkie al resto de los archivos html

--- js/login.js
+++ js/login.js
@@ -1,10 +1,25 @@
 const form = document.getElementById("formulario");
 
-form.addEventListener('submit',function(){
-    const datos = document.getElementsByClassName("datos");
+var ultimoSegmento = window.location.pathname.split('/').pop();//separo el final de la ruta en la que se esta parado
 
-    if(datos[0].value!=='' && datos[1].value!==''){
-        event.preventDefault();
-        window.location.href="index.html";
-    }    
-});
+if (ultimoSegmento == 'login.html') {
+    form.addEventListener('submit', function (event) {
+        const datos = document.getElementsByClassName("datos");
+
+        if (datos[0].value !== '' && datos[1].value !== '') {
+            event.preventDefault();
+            window.location.href = "index.html";
+            sessionStorage.setItem("logueo", "true");
+        }
+    });
+}
+
+function verificarLogueo() {
+
+    if (ultimoSegmento !== 'login.html' && sessionStorage.getItem("logueo") !== "true") {
+            window.location.href = "login.html";
+        }
+    
+}
+window.onload = verificarLogueo;
+
