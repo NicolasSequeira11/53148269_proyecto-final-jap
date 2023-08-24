05decc5a1f360a931ee6cea0bb4669928b6ea747
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-08-23 21:27:58 -0300

    Cambio products.js

--- js/products.js
+++ js/products.js
@@ -7,48 +7,22 @@ const url = ("https://japceibal.github.io/emercado-api/cats_products/"+ id +".js
         .then(response => response.json())
         .then(data => {
             const products = data.products; // Constante para trabajar sobre la info de cada producto.
+            let content = "";
             products.forEach(product => {
-                const containerCard = document.createElement("div");
-                containerCard.classList.add("col-xl-4", "col-12", "col-md-6", "col-lg-3", "container-products");
-
-                // CREAR ELEMENTO DIV (CONTENEDOR PRODUCTO)
-                const productDiv = document.createElement("div");
-                productDiv.classList.add("card", "col-12", "div-products");
-
-                // CREAR ELEMENTO IMAGEN
-                const image = document.createElement("img");
-                image.src = product.image;
-                image.classList.add("card-image", "image-products");
-                productDiv.appendChild(image);
-
-                // CREAR ELEMENTO H2 (TITULO)
-                const name = document.createElement("h2");
-                name.textContent = product.name;
-                name.classList.add("card-title", "title-products");
-                productDiv.appendChild(name);
-
-                // CREAR ELEMENTO P (DESCRIPCIÓN)
-                const description = document.createElement("p");
-                description.textContent = product.description;
-                description.classList.add("card-description", "description-products");
-                productDiv.appendChild(description);
-
-                // CREAR ELEMENTO P (COSTO PRODUCTO)
-                const cost = document.createElement("p");
-                cost.textContent = "Precio: " + product.currency + "" + product.cost;
-                cost.classList.add("card-cost", "cost-products");
-                productDiv.appendChild(cost);
-
-                // CREAR ELEMENTO P (CANTIDAD VENDIDOS)
-                const soldCount = document.createElement("p");
-                soldCount.textContent = "Cantidad vendida: " + product.soldCount;
-                soldCount.classList.add("card-soldCount", "soldCount-products");
-                productDiv.appendChild(soldCount);
-
-                // AGREGAR ELEMENTOS AL HTML PARA PODER VISUALIZARLOS
-                containerCard.appendChild(productDiv);
-                container.appendChild(containerCard);
-            });
+                content += 
+                `
+                <div class="col-xl-4 col-12 col-md-6 col-lg-3 container-products">
+                    <div class="card col-12 div-products">
+                        <img class="card-image image-products" src="${product.image}">
+                        <h2 class="card-title title-products">${product.name}</h2>
+                        <p class="card-description description-products">${product.description}</p>
+                        <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
+                        <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
+                    </div>
+                </div>
+                `;
+                container.innerHTML = content;
+                });
         })
         // Mensaje de error por si ocurre un error al cargar el fetch.
         .catch(error => {
