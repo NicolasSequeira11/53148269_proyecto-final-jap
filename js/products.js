48c8bc6d4b0009036944554f52d6160c951e5715
martip12 <martina.pereira1209@gmail.com>
2023-08-26 20:06:27 -0300

    buscargr4

--- js/products.js
+++ js/products.js
@@ -1,30 +1,67 @@
-const container = document.querySelector("#container-content"); // Constante que toma el contenedor div del HTML.
+const container = document.querySelector("#container-content");
 
-const id = localStorage.getItem("catID");
-const url = ("https://japceibal.github.io/emercado-api/cats_products/"+ id +".json"); // URL con los JSON de todas las categorías
+const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const products = data.products; // Constante para trabajar sobre la info de cada producto.
-            let content = "";
+            const products = data.products;
             products.forEach(product => {
-                content += 
-                `
-                <div class="col-xl-4 col-12 col-md-6 col-lg-3 container-products">
-                    <div class="card col-12 div-products">
-                        <img class="card-image image-products" src="${product.image}">
-                        <h2 class="card-title title-products">${product.name}</h2>
-                        <p class="card-description description-products">${product.description}</p>
-                        <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
-                        <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
-                    </div>
-                </div>
-                `;
-                container.innerHTML = content;
-                });
+                const containerCard = document.createElement("div");
+                containerCard.classList.add("col-xl-4", "col-12", "col-md-6", "col-lg-3", "container-products");
+
+                const productDiv = document.createElement("div");
+                productDiv.classList.add("card", "col-12", "div-products");
+
+                const image = document.createElement("img");
+                image.src = product.image;
+                image.classList.add("card-image", "image-products");
+                productDiv.appendChild(image);
+
+                const name = document.createElement("h2");
+                name.textContent = product.name;
+                name.classList.add("card-title", "title-products");
+                productDiv.appendChild(name);
+
+                const description = document.createElement("p");
+                description.textContent = product.description;
+                description.classList.add("card-description", "description-products");
+                productDiv.appendChild(description);
+
+                const cost = document.createElement("p");
+                cost.textContent = "Precio: " + product.currency + "" + product.cost;
+                cost.classList.add("card-cost", "cost-products");
+                productDiv.appendChild(cost);
+
+                const soldCount = document.createElement("p");
+                soldCount.textContent = "Cantidad vendida: " + product.soldCount;
+                soldCount.classList.add("card-soldCount", "soldCount-products");
+                productDiv.appendChild(soldCount);
+
+                containerCard.appendChild(productDiv);
+                container.appendChild(containerCard);
+            });
         })
-        // Mensaje de error por si ocurre un error al cargar el fetch.
         .catch(error => {
             console.error("Error al cargar los productos:", error);
         });
+        function filtrar() {
+            const input = document.getElementById("buscar");
+            let card = document.getElementsByClassName("col-xl-4 col-12 col-md-6 col-lg-3 container-products");
+            let nombres = document.getElementsByClassName("card-title title-products");// Obtener el texto del elemento en mayusculas
+            let descripciones = document.getElementsByClassName("card-description description-products");
+                
+            // Mostrar u ocultar el elemento según si coincide con el texto ingresado
+            for(let i=0; i<nombres.length; i++){
+                let descripcion = descripciones[i].textContent.toUpperCase();
+                let nombre = nombres[i].textContent.toUpperCase();
+        
+                if (descripcion.includes(input.value.toUpperCase()) || 
+                    nombre.includes( input.value.toUpperCase() )) 
+                {
+                    card[i].style.display = 'block'; // Mostrar el elemento
+                } else {
+                    card[i].style.display = 'none'; // Ocultar el elemento
+                }
+            }
+        }
