d9e1fcd324fb52f3def8fba224ba5e6b96330217
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-08-28 01:58:05 -0300

    corrección fetch

--- js/products.js
+++ js/products.js
@@ -1,50 +1,35 @@
-const container = document.querySelector("#container-content");
+const container = document.querySelector("#container-content"); // Constante que toma el contenedor div del HTML.
 
-const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const id = localStorage.getItem("catID");
+const url = ("https://japceibal.github.io/emercado-api/cats_products/"+ id +".json"); // URL con los JSON de todas las categorías
 
-    fetch(url)
+        fetch(url)
         .then(response => response.json())
         .then(data => {
-            const products = data.products;
+            const products = data.products; // Constante para trabajar sobre la info de cada producto.
+            let content = "";
             products.forEach(product => {
-                const containerCard = document.createElement("div");
-                containerCard.classList.add("col-xl-4", "col-12", "col-md-6", "col-lg-3", "container-products");
-
-                const productDiv = document.createElement("div");
-                productDiv.classList.add("card", "col-12", "div-products");
-
-                const image = document.createElement("img");
-                image.src = product.image;
-                image.classList.add("card-image", "image-products");
-                productDiv.appendChild(image);
-
-                const name = document.createElement("h2");
-                name.textContent = product.name;
-                name.classList.add("card-title", "title-products");
-                productDiv.appendChild(name);
-
-                const description = document.createElement("p");
-                description.textContent = product.description;
-                description.classList.add("card-description", "description-products");
-                productDiv.appendChild(description);
-
-                const cost = document.createElement("p");
-                cost.textContent = "Precio: " + product.currency + "" + product.cost;
-                cost.classList.add("card-cost", "cost-products");
-                productDiv.appendChild(cost);
-
-                const soldCount = document.createElement("p");
-                soldCount.textContent = "Cantidad vendida: " + product.soldCount;
-                soldCount.classList.add("card-soldCount", "soldCount-products");
-                productDiv.appendChild(soldCount);
-
-                containerCard.appendChild(productDiv);
-                container.appendChild(containerCard);
-            });
+                content += 
+                `
+                <div class="col-xl-4 col-12 col-md-6 col-lg-3 container-products">
+                    <div class="card col-12 div-products">
+                        <img class="card-image image-products" src="${product.image}">
+                        <h2 class="card-title title-products">${product.name}</h2>
+                        <p class="card-description description-products">${product.description}</p>
+                        <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
+                        <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
+                    </div>
+                </div>
+                `;
+                container.innerHTML = content;
+                });
         })
+        // Mensaje de error por si ocurre un error al cargar el fetch.
         .catch(error => {
-            console.error("Error al cargar los productos:", error);
+            console.error("Error al cargar los productos:", error);b
         });
+
+        //FILTRAR
         function filtrar() {
             const input = document.getElementById("buscar");
             let card = document.getElementsByClassName("col-xl-4 col-12 col-md-6 col-lg-3 container-products");
