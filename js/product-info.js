ff98a612ae8f36ca6f899b527d84ed7955801402
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-09-22 16:24:05 -0300

    Reestructura de products.js y products-info.js con nuevas funciones y comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,154 +1,154 @@
-const containerProduct = document.getElementById("container-product"); // Constante que toma el contenedor div del HTML.
-const containerComment = document.getElementById("container-comments"); 
+const containerProduct = document.getElementById("container-product"); // Obtener el div para agregar la info del producto
+const containerComment = document.getElementById("container-comments"); // Obtener el div para agregar los comentarios del producto
 
+const productID = localStorage.getItem("productID"); // Obtener valor del localStorage productID
+const urlProduct = "https://japceibal.github.io/emercado-api/products/" + productID + ".json"; // URL con los JSON de los productos
+const urlComment = "https://japceibal.github.io/emercado-api/products_comments/" + productID + ".json"; // URL con los JSON de los comentarios
 
-const productID = localStorage.getItem("productID"); // Constante que toma el valor del localStorage productID
-const urlProduct = ("https://japceibal.github.io/emercado-api/products/" + productID + ".json"); // URL con los JSON de los productos
-const urlComment =  ("https://japceibal.github.io/emercado-api/products_comments/" + productID + ".json"); // URL con los JSON de los comentarios
+/* ---------- FETCH PRODUCT INFO ---------- */
 
-        // FETCH URLProduct
+fetch(urlProduct)
+  .then((response) => response.json())
+  .then((data) => {
+    let productInfo = data; // Obtener la info de cada producto
+    showProductInfo(productInfo);
+  })
+  /* Mensaje por si ocurre un error al cargar el fetch */
+  .catch((error) => {
+    console.error("Error al cargar los productos:", error);
+  });
 
-        fetch(urlProduct)
-        .then(response => response.json())
-        .then(data => {
-            let productInfo = data; // Variable para acceder a la info de cada producto.
-            
-            // Mostrar info de productos en product-info.html
-            containerProduct.innerHTML = 
-            `
-            <div class="col-12 row">
-                <div class="py-5 card col-12 col-lg-7">
-                    
-                    <div id="carouselExample" class="carousel slide">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="${productInfo.images[0]}" class="d-block w-100" alt="..."> 
-                            </div>
-                            <div class="carousel-item">
-                                <img src="${productInfo.images[1]}" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="${productInfo.images[2]}" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="${productInfo.images[3]}" class="d-block w-100" alt="...">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon btn btn-light bg-primary" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                            <span class="carousel-control-next-icon btn btn-light bg-primary" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
+/* ---------- FETCH COMMENTS ---------- */
 
-                </div>
+let commentHTML = "";
 
-                <div class="col-12 col-lg-5 container-product-info">
-                    <h2 class="card-title title-product-info"><strong>${productInfo.name}</strong></h2>
-                    <p class="card-description description-product-info">${productInfo.description}</p>
-                    <p class="card-description category-product-info"><strong>Categoría</strong> <br> ${productInfo.category}</p>
-                    <p class="card-soldcount soldCount-product-info"><strong>Cantidad de vendidos</strong> <br> ${productInfo.soldCount}</p>
-                    <p class="card-cost cost-product-info"><strong>Precio</strong> <br> ${productInfo.currency} ${productInfo.cost}</p>
-                </div>
-            </div>
+fetch(urlComment)
+  .then((response) => response.json())
+  .then((data) => {
+    let commentInfo = data; // Obtener la info de cada producto
+    showComments(commentInfo);
+  });
 
-            `;
+/* Evento para añadir el nuevo comentario */
+const url = "https://jsonplaceholder.typicode.com/users"; // API para enviar datos del formulario
 
-        })
-        // Mensaje de error por si ocurre un error al cargar el fetch.
-        .catch(error => {
-            console.error("Error al cargar los productos:", error);
-        });
+let hoy = new Date(); // Obtener fecha actual
+let date = hoy.toLocaleString("en-US"); // Formatear fecha
 
-        //FIN FETCH URLProduct
+document.getElementById("commentForm").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-        //FETCH URLComment
-        let commentHTML = '';
+  let newCommentHTML = ""; // Contenedor vacío en donde añadir nuevos comentarios
 
-        fetch(urlComment)
-        .then(response => response.json())
-        .then(data => {
-            let commentInfo = data; // Variable para acceder a la info de cada producto.
-
-            commentInfo.forEach(element => {
-                commentHTML += `
-                <div class="comment col-lg-7 ps-4">
-                    <p class="py-2 m-auto mt-3"><strong>${element.user}</strong> - ${element.dateTime} -
-            `
-                for(let i=1; i<=5; i++){
-                    if(i<=element.score){
-                        commentHTML += `<span class="fa fa-star checked"></span>`
-                    }else{
-                        commentHTML += `<span class="fa fa-star"></span>`
-                    }
-                }
-            
-                commentHTML += `    
-                    </p>
-                    <p class="desc m-auto py-2 mb-3">${element.description} </p>
-                </div>
-                `
-            });
-            containerComment.innerHTML += commentHTML;
-        
-        })
-        // AGREGAR NUEVO COMENTARIO
-
-        const formComment = document.getElementById("commentForm"); // Obtener el formulario.
-        const url = "https://jsonplaceholder.typicode.com/users"; // API para enviar datos del formulario
-
-        let hoy = new Date(); // Variable que obtiene la fecha actual.
-        let date = hoy.toLocaleString("en-US"); // Variable para formatear la fecha.
-
-        // Evento para añadir el nuevo comentario.
-        formComment.addEventListener("submit", e => {
-            e.preventDefault();
-
-            const opinion = document.getElementById("comment").value; // Obtener contenido de la opinión.
-            const puntuacion = document.getElementById("rating").value; // Obtener valor de la puntuación.
-            const datos = localStorage.getItem("usuario"); // Obtener el nombre de usuario del login.
-            
-            let newCommentHTML = ''; // Crear contenedor vacío en donde añadir nuevos comentarios.
-
-            fetch(url, {
-                method: "POST",
-                body: JSON.stringify({
-                    score: puntuacion,
-                    description: opinion,
-                    user: datos,
-                    dateTime: date,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-            .then(response => response.json())
-            .then(datos => {
-
-                //Añadir elementos HTML del nuevo comentario
-                newCommentHTML += 
-                    `
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      score: document.getElementById("rating").value, // Puntuación
+      description: document.getElementById("comment").value, // Comentario
+      user: localStorage.getItem("usuario"), // Usuario
+      dateTime: date,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((datos) => {
+      /* Añadir elementos HTML del nuevo comentario */
+      newCommentHTML += `
                     <div class="comment ps-4">
                         <p class="py-2 m-auto mt-3"><strong>${datos.user}</strong> - ${datos.dateTime} -
-                    `
-                    for(let i=1; i<=5; i++){
-                        if(i<=datos.score){
-                            newCommentHTML += `<span class="fa fa-star checked"></span>`
-                        }else{
-                            newCommentHTML += `<span class="fa fa-star"></span>`
-                        }
-                    }
-                    newCommentHTML += 
-                    ` 
+                    `;
+      for (let i = 1; i <= 5; i++) {
+        if (i <= datos.score) {
+          newCommentHTML += `<span class="fa fa-star checked"></span>`;
+        } else {
+          newCommentHTML += `<span class="fa fa-star"></span>`;
+        }
+      }
+      newCommentHTML += ` 
                         </p>
                         <p class="desc m-auto py-2 mb-3">${datos.description} </p>
                     </div>
-                    `
-                    containerComment.innerHTML += newCommentHTML;
-            })
-        });
+                    `;
+      containerComment.innerHTML += newCommentHTML;
+    });
+});
 
-        // FIN AGREGAR NUEVO COMENTARIO
+/* ---------- FUNCIONES ---------- */
+
+/* Mostrar info del producto */
+function showProductInfo(array) {
+  containerProduct.innerHTML = `
+    <div class="col-12 row">
+        <div class="py-5 card col-12 col-lg-7">
+                    
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="${array.images[0]}" class="d-block w-100" alt="..."> 
+                    </div>
+                    <div class="carousel-item">
+                        <img src="${array.images[1]}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="${array.images[2]}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="${array.images[3]}" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon btn btn-light bg-primary" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon btn btn-light bg-primary" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+
+        <div class="col-12 col-lg-5 container-product-info">
+            <h2 class="card-title title-product-info"><strong>${array.name}</strong></h2>
+            <p class="card-description description-product-info">${array.description}</p>
+            <p class="card-description category-product-info"><strong>Categoría</strong> <br> ${array.category}</p>
+            <p class="card-soldcount soldCount-product-info"><strong>Cantidad de vendidos</strong> <br> ${array.soldCount}</p>
+            <p class="card-cost cost-product-info"><strong>Precio</strong> <br> ${array.currency} ${array.cost}</p>
+        </div>
+    </div>
+  `;
+}
+
+/* Mostrar comentarios */
+
+function showComments(array) {
+  array.forEach((element) => {
+    commentHTML += `
+              <div class="comment col-lg-7 ps-4">
+                  <p class="py-2 m-auto mt-3"><strong>${element.user}</strong> - ${element.dateTime} -
+          `;
+
+    addStars(element);
+
+    commentHTML += `    
+                  </p>
+                  <p class="desc m-auto py-2 mb-3">${element.description} </p>
+              </div>
+              `;
+  });
+  containerComment.innerHTML += commentHTML;
+}
+
+/* Agregar estrellas */
+
+function addStars(data) {
+  for (let i = 1; i <= 5; i++) {
+    if (i <= data.score) {
+      commentHTML += `<span class="fa fa-star checked"></span>`;
+    } else {
+      commentHTML += `<span class="fa fa-star"></span>`;
+    }
+  }
+}
--- js/products.js
+++ js/products.js
@@ -1,171 +1,158 @@
 const container = document.querySelector("#container-content"); // Constante que toma el contenedor div del HTML.
-const asc = document.getElementById("ascendente"); // Boton filtro precio ascendente
-const desc = document.getElementById("descendente"); // Boton filtro precio descendente
-const rel = document.getElementById("relevancia"); // Boton filtro relevancia descendente
-const rangoPrecio = document.getElementById("rangoPrecio"); // Boton filtro rango de precio
-const limpiarFiltros = document.getElementById("limpiarFiltros"); // Boton limpiar filtros
 
 const id = localStorage.getItem("catID");
-const url = ("https://japceibal.github.io/emercado-api/cats_products/"+ id +".json"); // URL con los JSON de todas las categorías
+const url =
+  "https://japceibal.github.io/emercado-api/cats_products/" + id + ".json"; // URL con los JSON de todas las categorías
 
+  
+
+/* ---------- FETCH ---------- */
+
+fetch(url)
+.then((response) => response.json())
+.then((data) => {
+  let products = data.products; // Constante para trabajar sobre la info de cada producto.
+  showProducts(products);
+
+  // FILTROS
+
+  /* Filtrar precio ascendente */
+  document.getElementById("ascendente").addEventListener("click", () => {
+    filtrarPrecioAscendente(products, products);
+  });
+
+  /* Filtrar precio descendente */
+  document.getElementById("descendente").addEventListener("click", () => {
+    filtrarPrecioDescendente(products, products);
+  });
+
+  /* Filtrar por relevancia descendente */
+  document.getElementById("relevancia").addEventListener("click", () => {
+    filtrarRelevanciaDescendente(products, products);
+  });
+
+  /* Filtrar por rango de precio */
+  document.getElementById("rangoPrecio").addEventListener("click", () => {
+    filtrarRangoPrecio(products);
+  });
+
+  /* Limpiar filtros */
+  document.getElementById("limpiarFiltros").addEventListener("click", () => {
+    showProducts(products);
+  });
+
+  // FIN FILTROS
+
+})
+/* Mensaje de error por si ocurre un error al cargar el fetch */
+.catch((error) => {
+  console.error("Error al cargar los productos:", error);
+});
+
+
+
+/* ---------- FUNCIONES ---------- */
+
+/* Crear un localStorage para guardar el id de cada producto y usarlo al clickear en el */
 function setProductID(id) {
-    localStorage.setItem("productID", id); // Crea el localStorage con la key "productID"
-    window.location = "product-info.html" // Redirige a product-info.html
+  localStorage.setItem("productID", id); // Crea el localStorage con la key "productID"
+  window.location = "product-info.html"; // Redirige a product-info.html
 }
 
-        // FETCH
+/* Mostrar productos */
+function showProducts(array) {
+  let content = "";
 
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let products = data.products; // Constante para trabajar sobre la info de cada producto.
-            
-            // Función que muestra los productos
-            function showProducts(array){
-                let content = "";
-            
-            if(array.length > 0){
-                array.forEach(product => {
-                    content += 
-                    `
-                    <div onclick="setProductID(${product.id})" class="col-xl-4 col-12 col-md-6 col-lg-3 cursor-active container-products">
-                        <div class="card col-12 div-products">
-                            <img class="card-image image-products" src="${product.image}">
-                            <h2 class="card-title title-products">${product.name}</h2>
-                            <p class="card-description description-products">${product.description}</p>
-                            <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
-                            <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
-                        </div>
+  if (array.length > 0) {
+    array.forEach((product) => {
+      content += `
+                <div onclick="setProductID(${product.id})" class="col-xl-4 col-12 col-md-6 col-lg-3 cursor-active container-products">
+                    <div class="card col-12 div-products">
+                        <img class="card-image image-products" src="${product.image}">
+                        <h2 class="card-title title-products">${product.name}</h2>
+                        <p class="card-description description-products">${product.description}</p>
+                        <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
+                        <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
                     </div>
-                    `;
-                container.innerHTML = content;
-                });
-                
-            } else {
-                // Alerta para cuando no se encuentran productos
-                container.innerHTML = 
-                    `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
-            }
-            }
+                </div>
+                `;
+      container.innerHTML = content;
+    });
+  } else {
+    // Alerta para cuando no se encuentran productos
+    container.innerHTML = `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
+  }
+}
 
-            showProducts(products);
+/* Filtrar precio ascendente */
+function filtrarPrecioAscendente(array, arrayShow) {
+  array.sort((a, b) => {
+    return a.cost - b.cost;
+  });
+  showProducts(arrayShow);
+}
 
+/* Filtrar precio descendente */
+function filtrarPrecioDescendente(array, arrayShow) {
+  array.sort((a, b) => {
+    return b.cost - a.cost;
+  });
+  showProducts(arrayShow);
+}
 
-            // FILTROS
+/* Filtrar relevancia descendente */
+function filtrarRelevanciaDescendente(array, arrayShow) {
+  array.sort((a, b) => {
+    return b.soldCount - a.soldCount;
+  });
+  showProducts(arrayShow);
+}
 
-            // Filtrar precio ascendente
-            asc.addEventListener("click", ()=>{
-                
-                products.sort((a, b) => {return a.cost - b.cost});
+/* Filtrar rango de precio */
+function filtrarRangoPrecio(array) {
+  let productosRangoPrecio = array.filter(
+    (product) =>
+      product.cost >= document.getElementById("precioMinimo").value &&
+      product.cost <= document.getElementById("precioMaximo").value
+  );
+  document.getElementById("precioMinimo").value = ""; // Limpiar input
+  document.getElementById("precioMaximo").value = ""; // Limpiar input
+  showProducts(productosRangoPrecio);
 
-                showProducts(products);
+  /* Filtro precio ascendente luego de filtrar por precio */
+  document.getElementById("ascendente").addEventListener("click", () => {
+    filtrarPrecioAscendente(productosRangoPrecio, productosRangoPrecio);
+  });
 
-            });
+  /* Filtro precio descendente luego de filtrar por precio */
+  document.getElementById("descendente").addEventListener("click", () => {
+    filtrarPrecioDescendente(productosRangoPrecio, productosRangoPrecio);
+  });
 
+  /* Filtro relevancia descendente luego de filtrar por precio */
+  document.getElementById("relevancia").addEventListener("click", () => {
+    filtrarRelevanciaDescendente(productosRangoPrecio, productosRangoPrecio);
+  });
+}
 
-            // Filtrar precio descendente
-            desc.addEventListener("click", ()=>{
+/* Buscador de productos */
+function buscarProductos() {
+  let card = document.getElementsByClassName("col-xl-4 col-12 col-md-6 col-lg-3 container-products");
+  let nombres = document.getElementsByClassName("card-title title-products"); // Obtener el texto del elemento en mayusculas
+  let descripciones = document.getElementsByClassName("card-description description-products");
 
-                products.sort((a, b) => {return b.cost - a.cost});
+  /* Mostrar u ocultar el elemento según si coincide con el texto ingresado */
+  for (let i = 0; i < nombres.length; i++) {
+    let descripcion = descripciones[i].textContent.toUpperCase();
+    let nombre = nombres[i].textContent.toUpperCase();
 
-                showProducts(products);
-
-            });    
-
-
-            // Filtrar por relevancia descendente
-            rel.addEventListener("click", ()=>{
-
-                products.sort((a, b) => {return b.soldCount - a.soldCount});
-
-                showProducts(products);
-
-            });    
-
-
-            // Filtrar por rango de precio
-            rangoPrecio.addEventListener("click", ()=>{
-
-                let precioMin = document.getElementById("precioMinimo");
-                let precioMax = document.getElementById("precioMaximo");
-
-                let productosRangoPrecio =      
-                products.filter((product) => product.cost >= precioMin.value && product.cost <= precioMax.value);
-
-                showProducts(productosRangoPrecio);
-
-                //Filtro precio ascendente luego de filtrar por precio
-                asc.addEventListener("click", ()=> {
-
-                    let precioAscendente =   
-                    productosRangoPrecio.sort((a, b) => {return a.cost - b.cost});
-    
-                    showProducts(precioAscendente);
-
-                });
-
-                //Filtro precio descendente luego de filtrar por precio
-                desc.addEventListener("click", ()=> {
-
-                    let precioDescendente =   
-                    productosRangoPrecio.sort((a, b) => {return b.cost - a.cost});
-    
-                    showProducts(precioDescendente);
-
-                });
-
-                //Filtro relevancia descendente luego de filtrar por precio
-                rel.addEventListener("click", ()=> {
-
-                    let relevanciaDescendente =   
-                    productosRangoPrecio.sort((a, b) => {return b.soldCount - a.soldCount});
-    
-                    showProducts(relevanciaDescendente);
-
-                });
-
-            });
-
-
-            // Limpiar filtros
-            limpiarFiltros.addEventListener("click", ()=>{
-
-                showProducts(products);
-
-            }); 
-            
-            // FIN FILTROS
-            
-        })
-        // Mensaje de error por si ocurre un error al cargar el fetch.
-        .catch(error => {
-            console.error("Error al cargar los productos:", error);
-        });
-
-        //FIN FETCH
-
-
-        // BUSCADOR DE PRODUCTOS
-
-        function buscarProductos() {
-            const input = document.getElementById("buscar"); // Obtener input search
-            let card = document.getElementsByClassName("col-xl-4 col-12 col-md-6 col-lg-3 container-products");
-            let nombres = document.getElementsByClassName("card-title title-products"); // Obtener el texto del elemento en mayusculas
-            let descripciones = document.getElementsByClassName("card-description description-products");
-            
-            // Mostrar u ocultar el elemento según si coincide con el texto ingresado
-            for(let i=0; i<nombres.length; i++){
-                let descripcion = descripciones[i].textContent.toUpperCase();
-                let nombre = nombres[i].textContent.toUpperCase();
-        
-                if (descripcion.includes(input.value.toUpperCase()) || 
-                    nombre.includes( input.value.toUpperCase() )) 
-                {
-                    card[i].style.display = 'block'; // Mostrar el elemento
-                } else {
-                    card[i].style.display = 'none'; // Ocultar el elemento
-                }
-            }
-        }
-        
-        // FIN BUSCADOR DE PRODUCTOS
+    if (
+      descripcion.includes(document.getElementById("buscar").value.toUpperCase()) ||
+      nombre.includes(document.getElementById("buscar").value.toUpperCase())
+    ) {
+      card[i].style.display = "block"; // Mostrar el elemento
+    } else {
+      card[i].style.display = "none"; // Ocultar el elemento
+    }
+  }
+}
