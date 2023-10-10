b4e327444437116c5d9c85f296f47d713f4393e6
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-10-10 10:56:12 -0300

    if en showNewArticle()

--- js/cart.js
+++ js/cart.js
@@ -20,40 +20,76 @@ function showArticles(array) {
   if (array.length > 0) {
     array.forEach((article) => {
       containerCart.innerHTML += `
-          <hr>
+      <hr>
 
-          <div class="article-container">
+      <div class="article-container">
 
-            <div class="col-2">
-              <img src="${article.image}" class="col-12">
-            </div>
+        <div class="col-2">
+          <img src="${article.image}" class="col-12">
+        </div>
 
-            <div class="col-10 d-flex m-auto">
-            
-              <div class="d-flex col-6 col-md-7">
-                <p class="my-auto ps-2">${article.name}</p>
-              </div>
-
-              <div class="d-flex col-1 col-md-2">
-                <input type="number" class="m-auto col-md-8 col-12 d-flex article-input" 
-                  min="0" id="count" value="${article.count}"></p>
-              </div>
-
-              <div class="d-flex col-5 col-md-3">
-                <p class="m-auto ps-2">${article.unitCost} ${article.currency}</p>
-              </div>
-
-            </div>
-                          
+        <div class="col-10 d-flex m-auto">
+        
+          <div class="d-flex col-6 col-md-7">
+            <p class="my-auto ps-2">${article.name}</p>
           </div>
 
+          <div class="d-flex col-1 col-md-2">
+            <input type="number" class="m-auto col-md-8 col-12 d-flex article-input" 
+              min="0" id="count" value="${article.count}"></p>
+          </div>
 
-                      `;
+          <div class="d-flex col-5 col-md-3">
+            <p class="m-auto ps-2">${article.unitCost} ${article.currency}</p>
+          </div>
+
+        </div>
+                          
+      </div>`;
     });
+
+    showNewArticle();
   } else {
     // Alerta para cuando no se encuentran productos
     containerCart.innerHTML += `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
   }
+}
+
+function showNewArticle() {
+    const getName = localStorage.getItem("productName");
+    const getPrice = localStorage.getItem("productPrice");
+    const getImage = localStorage.getItem("productImage");
+
+    if(getName || getPrice || getImage){
+      containerCart.innerHTML += `
+      <hr>
+
+      <div class="article-container">
+
+        <div class="col-2">
+          <img src="${getImage}" class="col-12">
+        </div>
+
+        <div class="col-10 d-flex m-auto">
+            
+          <div class="d-flex col-6 col-md-7">
+            <p class="my-auto ps-2">${getName}</p>
+          </div>
+
+          <div class="d-flex col-1 col-md-2">
+            <input type="number" class="m-auto col-md-8 col-12 d-flex article-input" 
+              min="0" id="count" value="1"></p>
+          </div>
+
+          <div class="d-flex col-5 col-md-3">
+            <p class="m-auto ps-2">${getPrice} USD</p>
+          </div>
+
+        </div>
+                          
+      </div>
+    `;
+    }
 }
 
 function showTotal(array) {
@@ -61,15 +97,15 @@ function showTotal(array) {
   if (array.length > 0) {
     array.forEach((article) => {
       containerTotal.innerHTML += `
-                      <div class="article-container d-flex">
-                        <div class="d-flex col-6">
-                          <p class="my-auto">Total</p>
-                        </div>
-                        <div class="d-flex col-6">
-                          <p class="d-flex col-12 justify-content-end my-auto">
-                            ${article.unitCost} ${article.currency}</p>
-                        </div>
-                      </div>
+        <div class="article-container d-flex">
+          <div class="d-flex col-6">
+            <p class="my-auto">Total</p>
+          </div>
+          <div class="d-flex col-6">
+            <p class="d-flex col-12 justify-content-end my-auto">
+              ${article.unitCost} ${article.currency}</p>
+          </div>
+        </div>
                       `;
     });
   } else {
