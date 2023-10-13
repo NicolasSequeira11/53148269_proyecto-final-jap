c13e23a76efe3ea56c5d0d69f3c7a069e4768f1e
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-10-13 12:19:06 -0300

    Actualizar precio cuando se aumenta la cantidad del articulo

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,7 @@
 const urlCart = "https://japceibal.github.io/emercado-api/user_cart/25801.json"; // URL con los JSON de los productos
-const containerCart = document.getElementById("containerCart");
-const containerTotal = document.getElementById("cart-total");
-console.log(containerCart);
+const containerCart = document.getElementById("containerCart"); // Contenedor del carrito
+const containerTotal = document.getElementById("cart-total"); // Contenedor del total del carrito
+
 
 fetch(urlCart)
   .then((response) => response.json())
@@ -12,6 +12,23 @@ fetch(urlCart)
 
     showTotal(allArticles);
     showArticles(allArticles);
+
+    /* Evento para limpiar el carrito */
+    const btnClearCart = document.getElementById("btnClearCart");
+
+    btnClearCart.addEventListener("click", ()=>{
+      localStorage.removeItem("cart");
+
+      // Limpiar la representación visual de los elementos del carrito
+      containerCart.innerHTML = ''; // Borrar todos los articulos
+      containerTotal.innerHTML = ''; // Borrar el total
+
+      // Añadir un mensaje indicando que el carrito está vacío
+      containerCart.innerHTML = 
+        `<div class="bg-danger text-white text-center p-3 rounded">
+          El carrito está vacío
+        </div>`;
+    });
   })
   .catch((error) => {
     console.error("Error al cargar los productos:", error);
@@ -23,35 +40,57 @@ function showArticles(array) {
     array.forEach((article) => {
       containerCart.innerHTML += `
       <hr>
-
       <div class="article-container" data-unitCost="${article.unitCost}">
-
         <div class="col-2">
           <img src="${article.image}" class="col-12">
         </div>
-
         <div class="col-10 d-flex m-auto">
-        
           <div class="d-flex col-6 col-md-7">
-            <p class="my-auto ps-2">${article.name}</p>
+            <p class="my-auto ps-2">${article.name}<br>${article.unitCost} USD</p>
           </div>
-
           <div class="d-flex col-1 col-md-2">
             <input type="number" class="m-auto col-md-8 col-12 d-flex article-input count" 
-              min="0" id="count" value="${article.count}"></p>
+              min="0" value="${article.count}" data-article-id="${article.id}">
           </div>
-
           <div class="d-flex col-5 col-md-3">
-            <p class="m-auto ps-2 article-price">${article.unitCost} ${article.currency}</p>
+            <p class="m-auto ps-2 article-price">${(article.unitCost * article.count).toFixed(2)} ${article.currency}</p>
           </div>
-
         </div>
-                          
       </div>`;
+
+      // Obtiene todos los elementos <input> con la clase "count"
+      const countInputs = document.querySelectorAll('.count');
+
+      // Agrega un evento input a cada elemento <input>
+      countInputs.forEach(input => {
+        input.addEventListener('input', (event) => {
+          updatePrice(event.target);
+        });
+      });
     });
   } else {
     containerCart.innerHTML += `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
   }
+}
+
+function updatePrice(element) {
+  // Encuentra el elemento padre del input
+  const articleContainer = element.closest('.article-container');
+
+  // Encuentra el elemento que muestra el precio en el mismo artículo
+  const priceElement = articleContainer.querySelector('.article-price');
+
+  // Obtiene el precio unitario del artículo desde el atributo de datos "data-unitCost"
+  const unitCost = parseFloat(articleContainer.getAttribute('data-unitCost'));
+
+  // Obtiene el nuevo valor de cantidad desde el input
+  const newQuantity = parseInt(element.value, 10);
+
+  // Calcula el nuevo precio multiplicando la cantidad por el precio unitario
+  const newPrice = (unitCost * newQuantity).toFixed(2);
+
+  // Actualiza el elemento que muestra el precio
+  priceElement.textContent = `${newPrice} USD`;
 }
 
 function showTotal(array) {
@@ -72,9 +111,11 @@ function showTotal(array) {
                       ${total.toFixed(2)} USD
                   </p>
               </div>
-          </div>`;
+          </div>
+          <button class="col-12 mt-3 btn bg-danger text-white" id="btnClearCart">Limpiar carrito</button>`;
   } else {
       containerTotal.innerHTML = `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
   }
 }
+
 
