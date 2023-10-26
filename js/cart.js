52c13272e501ec9b0c9cb90089707372c493d528
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-10-25 22:04:24 -0300

    corrección errores validación formulario

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,23 @@
 const urlCart = "https://japceibal.github.io/emercado-api/user_cart/25801.json"; // URL con los JSON de los productos
 const containerCart = document.getElementById("containerCart"); // Contenedor del carrito
 
-const premium = document.getElementById("premiumRad"); // input 
+const premium = document.getElementById("premiumRad"); // input
 const express = document.getElementById("expressRad");
 const standard = document.getElementById("standardRad");
 
 fetch(urlCart)
   .then((response) => response.json())
   .then((data) => {
-    const localStorageCart = JSON.parse(localStorage.getItem('cart')); // Datos del localStorage
-    
+    const localStorageCart = JSON.parse(localStorage.getItem("cart")); // Datos del localStorage
+
     showArticles(localStorageCart);
     totalCart();
-
   })
   .catch((error) => {
     console.error("Error al cargar los productos:", error);
   });
 
 function showArticles(array) {
-  
   if (array.length > 0) {
     array.forEach((article) => {
       containerCart.innerHTML += `
@@ -28,8 +26,8 @@ function showArticles(array) {
         <div class="col-12 d-flex flex-wrap m-auto">
 
           <div class="col-12 col-md-6 justify-content-center pb-2 pb-md-0 d-flex">
-            <div class="d-flex col-6 col-md-4">
-              <img src="${article.image}" class="col-12">
+            <div class="d-flex col-6 col-md-5">
+              <img src="${article.image}" class="col-12 ">
             </div>
             <div class="d-flex col-6 justify-content-center col-md-8">
               <p class="my-auto col-12 ps-5 ps-md-2">${article.name}<br>${article.unitCost} USD</p>
@@ -42,7 +40,7 @@ function showArticles(array) {
                 min="0" value="${article.count}" data-article-id="${article.id}">
             </div>
             <div class="d-flex col-4 col-md-6 justify-content-center my-auto">
-              <p class="m-auto article-price">${(article.unitCost * article.count)} ${article.currency}</p>
+              <p class="m-auto article-price">${article.unitCost * article.count} ${article.currency}</p>
             </div>
             <div class="d-flex col-4 col-md-2 justify-content-end my-auto">
               <button class="btn bg-danger" id="btnCartDelete" data-article-id="${article.id}">
@@ -55,44 +53,49 @@ function showArticles(array) {
       </div>`;
 
       // Obtiene todos los elementos <input> con la clase "count"
-      const countInputs = document.querySelectorAll('.count');
+      const countInputs = document.querySelectorAll(".count");
 
       // Agrega un evento input a cada elemento <input>
-      countInputs.forEach(input => {
-        input.addEventListener('input', (event) => {
+      countInputs.forEach((input) => {
+        input.addEventListener("input", (event) => {
           updatePrice(event.target);
         });
       });
-      
-      document.querySelectorAll('#btnCartDelete').forEach(function (button) {
-        button.addEventListener('click', function (event) {
-          // Llama a una función para eliminar el artículo del carrito.
+
+      document.querySelectorAll("#btnCartDelete").forEach(function (button) {
+        button.addEventListener("click", function (event) {
+          // Llamar a la función que elimina el artículo del carrito
           eliminarArticuloDelCarrito(article.id);
-          location.reload();
+          location.reload(); // Recargar la página
         });
       });
     });
   } else {
-    containerCart.innerHTML += `<div class="alert-danger bg-danger alert-error-filter m-auto mt-2 mb-4 w-100">El carrito esta vacío</div>`;
+    containerCart.innerHTML += `
+      <div class="alert-danger bg-danger alert-error-filter m-auto mt-2 mb-4 w-100">
+        El carrito esta vacío
+      </div>`;
   }
 }
 
 function eliminarArticuloDelCarrito(articleId) {
   // Obtener el array de artículos del carrito del localStorage
-  const carrito = JSON.parse(localStorage.getItem('cart')) || [];
+  const carrito = JSON.parse(localStorage.getItem("cart")) || [];
 
-  // Encontrar el índice del artículo a eliminar en el array 
+  // Encontrar el índice del artículo a eliminar en el array
   const articleIndex = carrito.findIndex((article) => article.id === articleId);
 
   if (articleIndex !== -1) {
-    // Eliminar el artículo del array 
+    // Eliminar el artículo del array
     carrito.splice(articleIndex, 1);
 
     // Actualizar el localStorage con el carrito modificado
-    localStorage.setItem('cart', JSON.stringify(carrito));
+    localStorage.setItem("cart", JSON.stringify(carrito));
 
     // Eliminar la representación del artículo en la página
-    const articleContainer = document.querySelector(`[data-article-id="${articleId}"]`);
+    const articleContainer = document.querySelector(
+      `[data-article-id="${articleId}"]`
+    );
     if (articleContainer) {
       articleContainer.remove();
     }
@@ -102,16 +105,19 @@ function eliminarArticuloDelCarrito(articleId) {
 // Calcular el total inicial
 let initialTotal = 0;
 function calculateInitialTotal(array) {
-  initialTotal = array.reduce((acc, article) => acc + article.unitCost * article.count, 0);
+  initialTotal = array.reduce(
+    (acc, article) => acc + article.unitCost * article.count,
+    0
+  );
 }
 
-function typePrices(total){
+function typePrices(total) {
   let shippingCost = 0;
-  if(premium.checked){
+  if (premium.checked) {
     shippingCost = total * 0.15;
-  } else if (express.checked){
+  } else if (express.checked) {
     shippingCost = total * 0.07;
-  } else if (standard.checked){
+  } else if (standard.checked) {
     shippingCost = total * 0.05;
   }
   return shippingCost;
@@ -125,19 +131,19 @@ standard.addEventListener("change", totalCart);
 // Actualiza el precio del producto según sus cantidades
 function updatePrice(element) {
   // Encuentra el elemento padre del input
-  const articleContainer = element.closest('.article-container');
+  const articleContainer = element.closest(".article-container");
 
   // Encuentra el elemento que muestra el precio en el mismo artículo
-  const priceElement = articleContainer.querySelector('.article-price');
+  const priceElement = articleContainer.querySelector(".article-price");
 
   // Obtiene el precio unitario del artículo desde el atributo de datos "data-unitCost"
-  const unitCost = parseFloat(articleContainer.getAttribute('data-unitCost'));
+  const unitCost = parseFloat(articleContainer.getAttribute("data-unitCost"));
 
   // Obtiene el nuevo valor de cantidad desde el input
   const newQuantity = parseInt(element.value);
 
   // Calcula el nuevo precio multiplicando la cantidad por el precio unitario
-  const newPrice = (unitCost * newQuantity);
+  const newPrice = unitCost * newQuantity;
 
   // Actualiza el elemento que muestra el precio
   priceElement.textContent = `${newPrice} USD`;
@@ -148,13 +154,13 @@ function updatePrice(element) {
 // Actualiza el precio total del carrito
 function totalCart() {
   // Encuentra todos los elementos de precio de artículos
-  const priceArticles = document.querySelectorAll('.article-price');
+  const priceArticles = document.querySelectorAll(".article-price");
 
   // Inicializar el total del carrito con la suma del precio de los articulos en el carrito
   let total = initialTotal;
 
   // Calcular el nuevo total sumando los precios de todos los artículos
-  priceArticles.forEach(priceElement => {
+  priceArticles.forEach((priceElement) => {
     total += parseFloat(priceElement.textContent);
   });
 
@@ -162,8 +168,7 @@ function totalCart() {
 
   // Actualizar el elemento que muestra el total del carrito
   const containerTotal = document.getElementById("cart-total");
-  containerTotal.innerHTML = 
-    `<div class="m-auto mb-3 d-flex justify-content-between">
+  containerTotal.innerHTML = `<div class="m-auto mb-3 d-flex justify-content-between">
       <p class="my-auto">Subtotal</p>
       <p class="my-auto">${total} USD</p>
     </div>
@@ -178,55 +183,80 @@ function totalCart() {
       <p class="my-auto">${newTotal} USD</p>
     </div>
     `;
-} 
+}
 
+  // VALIDACIÓN FORMULARIO CARRITO
 
 (() => {
-  'use strict'
+  "use strict";
 
-  const forms = document.querySelectorAll('.needs-validation');
+  const forms = document.querySelectorAll(".needs-validation"); // Obtener formulario
+  const tarjet = document.getElementById("tarjeta"); // Obtener metodo de pago tarjeta de credito
+  const trans = document.getElementById("transferencia"); // Obtener metodo de pago transferencia
+  const alertMetodoPago = document.getElementById("alert-metodoPago"); // Alerta metodo de pago
 
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      const tarjet = document.getElementById("tarjeta");
-      const trans = document.getElementById("transferencia");
+  // Inputs metodo de pago
+  const numTarjeta = document.getElementById("numTarjeta"); // Input numero de tarjeta
+  const cvv = document.getElementById("cvv"); // Input cvv
+  const vencTarjeta = document.getElementById("vencTarjeta"); // Input vencimiento tarjeta
+  const numCuenta = document.getElementById("numCuenta"); // Input numero de cuenta
 
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-        
-        if(tarjet.checked){
-          showRequired(document.getElementById("numTarjeta"));
-          showRequired(document.getElementById("cvv"));
-          showRequired(document.getElementById("vencTarjeta"));
-          deleteRequired(document.getElementById("numCuenta"));
-        }else if(trans.checked){
-          deleteRequired(document.getElementById("numTarjeta"));
-          deleteRequired(document.getElementById("cvv"));
-          deleteRequired(document.getElementById("vencTarjeta"));
-          showRequired(document.getElementById("numCuenta"));
+  // Deshabilitar y habilitar inputs de metodo de pago según cuál se selecciona
+  // disable = true (Habilita) = false (Deshabilita)
+
+  /* Tarjeta de crédito */
+  tarjet.addEventListener("change", () => {
+    numCuenta.disabled = true;
+
+    numTarjeta.disabled = false;
+    cvv.disabled = false;
+    vencTarjeta.disabled = false;
+  });
+
+  /* Transferencia */
+  trans.addEventListener("change", () => {
+    numTarjeta.disabled = true;
+    cvv.disabled = true;
+    vencTarjeta.disabled = true;
+
+    numCuenta.disabled = false;
+  });
+
+  // Evento submit del formulario
+  Array.from(forms).forEach((form) => {
+    form.addEventListener("submit", (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add("was-validated");
+
+          // Si no está seleccionado el metodo de pago
+          if (!tarjet.checked && !trans.checked) {
+            
+            // Mostrar alerta "Es necesario seleccionar el metodo de pago"
+            alertMetodoPago.classList.remove("d-none");
+
+            // Si está seleccionado el metodo de pago y se validan los inputs
+          } else if (
+            !(tarjet.checked && numTarjeta.value === "" && cvv.value === "" && vencTarjeta.value === "") ||
+            !(trans.checked && numCuenta.value === "")) {
+
+              // Ocultar alerta "Es necesario seleccionar el metodo de pago"
+              alertMetodoPago.classList.add("d-none");
+          }
+          
+        } else {
+
+          // Alerta para cuando se realiza la compra con exito
+          document.getElementById("cartDiv").innerHTML += `
+            <div class="alert alert-success mt-3" role="alert">
+              ¡Has comprado con exito!
+            </div>`;
+
+          form.classList.remove("was-validated");
         }
-      }else{
-        document.getElementById("cartDiv").innerHTML += `
-      <div class="alert alert-success mt-3" role="alert">
-        ¡Has comprado con exito!
-      </div>
-      `;
-        form.classList.remove("was-validated");
-      }
-
-      form.classList.add('was-validated')
-      }, false)
-  })
-
-})()
-
-function showRequired(elem){
-  elem.setAttribute("required", "required");
-}
-
-function deleteRequired(elem){
-  elem.removeAttribute("required");
-}
-
-
+      },
+      false
+    );
+  });
+})();
