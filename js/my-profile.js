6740dd7a94cdd5daa1fd590eb646247daf93f1d8
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-11-10 12:33:03 -0300

    cambios esteticos

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,7 +8,40 @@ const btnSave = document.getElementById("btnSaveProfile");
 
 const defaultImageURL = "https://cdn.icon-icons.com/icons2/3217/PNG/512/add_user_profile_person_avatar_icon_196535.png";
 
-//Validación del formulario con boostrap
+// Obtener datos del perfil del localStorage
+const FirstName = localStorage.getItem("FirstName");
+const SecondName = localStorage.getItem("SecondName");
+const FirstLastname = localStorage.getItem("FirstLastName");
+const SecondLastname = localStorage.getItem("SecondLastName");
+const Tel = localStorage.getItem("Tel");
+const imagenProfile = localStorage.getItem("profileImage");
+
+// Obtener elemento HTML de la imagen del perfil
+const imgProfile = document.getElementById("selectedProfileImage");
+
+// Valores a mostrar cuando se cargue la página
+document.addEventListener("DOMContentLoaded", ()=>{
+  profileEmail.value = emailLogin;
+  profileFirstName.value = FirstName;
+  profileSecondName.value = SecondName;
+  profileFirstLastname.value = FirstLastname;
+  profileSecondLastname.value = SecondLastname;
+  profileTel.value = Tel;
+
+  if(!imagenProfile){
+    return null;
+  } else {
+    imgProfile.setAttribute("src", imagenProfile);
+  }
+});
+
+// Eliminar imagen de perfil
+document.getElementById("deleteImgProfile").addEventListener("click", ()=>{
+  localStorage.removeItem("profileImage");
+  location.reload();
+});
+
+// Validación del formulario con boostrap
 (() => {
     'use strict';
     const profileForm = document.getElementById('profileForm');
@@ -22,57 +55,7 @@ const defaultImageURL = "https://cdn.icon-icons.com/icons2/3217/PNG/512/add_user
     }, false);
 })();
 
-btnSave.addEventListener("click", ()=>{
-
-    // Obtener inputs del perfil al momento del click
-    const profileFirstName = document.getElementById("profileFirstName").value;
-    const profileSecondName = document.getElementById("profileSecondName").value;
-    const profileFirstLastname = document.getElementById("profileFirstLastname").value;
-    const profileSecondLastname = document.getElementById("profileSecondLastname").value;
-    const profileTel = document.getElementById("profileTel").value;
-
-    localStorage.setItem("FirstName", profileFirstName);
-    localStorage.setItem("SecondName", profileSecondName);
-    localStorage.setItem("FirstLastName", profileFirstLastname);
-    localStorage.setItem("SecondLastName", profileSecondLastname);
-    localStorage.setItem("Tel", profileTel);
-
-    // Recuperar la imagen seleccionada
-    const selectedProfileImage = document.getElementById("selectedProfileImage");
-    const storedImage = localStorage.getItem("profileImage");
-
-    if (storedImage) {
-        selectedProfileImage.style.display = "block";
-        selectedProfileImage.src = storedImage;
-    } else {
-        // Si no hay imagen en localStorage, mostrar la imagen predeterminada
-        selectedProfileImage.src = defaultImageURL;
-    }
-})
-
-// Obtener datos del perfil del localStorage
-const FirstName = localStorage.getItem("FirstName");
-const SecondName = localStorage.getItem("SecondName");
-const FirstLastname = localStorage.getItem("FirstLastName");
-const SecondLastname = localStorage.getItem("SecondLastName");
-const Tel = localStorage.getItem("Tel");
-const imagenProfile = localStorage.getItem("profileImage");
-const imgProfile = document.getElementById("selectedProfileImage");
-
-
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    profileEmail.value = emailLogin; // El valor del input email obtiene el valor del email de login
-    
-    profileFirstName.value = FirstName;
-    profileSecondName.value = SecondName;
-    profileFirstLastname.value = FirstLastname;
-    profileSecondLastname.value = SecondLastname;
-    profileTel.value = Tel;
-    imgProfile.setAttribute("src", imagenProfile);
-});
-
-//Función para previsualizar la foto seleccionada por el usuario
+// Función para previsualizar la foto seleccionada por el usuario
 function seleccionarFotoPerfil() {
   const profileImageInput = document.getElementById("profileImageInput");
   const selectedProfileImage = document.getElementById("selectedProfileImage");
@@ -84,14 +67,45 @@ function seleccionarFotoPerfil() {
       const reader = new FileReader();
 
       reader.onload = function (e) {
-        imagenSeleccionada = e.target.result;// Guardar la imagen actual en una variable
+        imagenSeleccionada = e.target.result; // Guardar la imagen actual en una variable
         selectedProfileImage.src = imagenSeleccionada;
-        selectedProfileImage.style.display = "block"; //Mostrar la imagen seleccionada
-
-        localStorage.setItem("profileImage", imagenSeleccionada); //Guardar la imagen en localStorage 
+        selectedProfileImage.style.display = "block"; // Mostrar la imagen seleccionada
       };
 
       reader.readAsDataURL(file);
     }
-  });
+});
+
+// Evento del botón 'Guardar cambios'
+btnSave.addEventListener("click", ()=>{
+
+  // Obtener inputs del perfil al momento del click
+  const profileFirstName = document.getElementById("profileFirstName").value;
+  const profileSecondName = document.getElementById("profileSecondName").value;
+  const profileFirstLastname = document.getElementById("profileFirstLastname").value;
+  const profileSecondLastname = document.getElementById("profileSecondLastname").value;
+  const profileTel = document.getElementById("profileTel").value;
+
+  // Guardar valores en el localStorage
+  localStorage.setItem("FirstName", profileFirstName);
+  localStorage.setItem("SecondName", profileSecondName);
+  localStorage.setItem("FirstLastName", profileFirstLastname);
+  localStorage.setItem("SecondLastName", profileSecondLastname);
+  localStorage.setItem("Tel", profileTel); 
+  localStorage.setItem("profileImage", imagenSeleccionada); 
+  imgProfile.setAttribute("src", imagenProfile);
+
+  // Recuperar la imagen seleccionada
+  const selectedProfileImage = document.getElementById("selectedProfileImage");
+  const storedImage = localStorage.getItem("profileImage");
+
+  if (storedImage) {
+      selectedProfileImage.style.display = "block";
+      selectedProfileImage.src = storedImage;
+  } else {
+      // Si no hay imagen en localStorage, mostrar la imagen predeterminada
+      selectedProfileImage.src = defaultImageURL;
+  }
+});
+
 }
